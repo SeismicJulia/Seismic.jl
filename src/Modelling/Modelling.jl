deeba8e3957b0c7012c17d60a4b99f9d0d27df74
Juani <sabbione@ualberta.ca>
2016-06-03 18:16:37 -0600

    Change wavelets

--- src/Modelling/Modelling.jl
+++ src/Modelling/Modelling.jl
@@ -1,4 +1,12 @@
 export SeisLinearEvents,
-Ricker
+Ricker,
+Ormsby,
+Berlage,
+Kolmogoroff,
+Hamming
 include("SeisLinearEvents.jl")
 include("Ricker.jl")
+include("Ormsby.jl")
+include("Berlage.jl")
+include("Kolmogoroff.jl")
+include("Hamming.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,7 @@ include("test_POCS.jl")
 include("test_MWNI.jl")
 include("test_FFTOp.jl")
 include("test_CG.jl")
+include("test_wavelets.jl")
+include("test_Kolmogoroff.jl")
 #include("test_CG_memory_vs_disk.jl")
 
