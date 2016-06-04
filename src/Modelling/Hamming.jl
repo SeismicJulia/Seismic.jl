c83675b40154e8764ba026075903cecba45bbe64
Juan I. Sabbione <sabbione@ualberta.ca>
2016-06-03 19:12:13 -0600

    Update wavelets (#21)
    * Small fix in SegyStruct. Working on SeisLinearEvents
    
    * still working on SeisLinearEvents
    
    * Commiting a grup of changes in SeisLinearEvents, etc
    
    * Change wavelets
    
    * Change wavelets
    
    * Removed a couple of tests from runtests.jl

---
+++ src/Modelling/Hamming.jl
@@ -0,0 +1,26 @@
+"""
+    Hamming(n)
+
+Create a Hamming window.
+
+# Arguments
+
+* `n::Int`: number of samples
+
+# Example
+```julia
+julia> w = Hamming(51); plot(w);
+```
+"""
+
+function Hamming(n::Int)
+
+    if n==1
+        return [1.]
+    end
+
+    n -= 1
+    k = collect(0:1:n)
+    hamm = 0.54 - 0.46*cos(2pi.*k/n)
+    
+end
