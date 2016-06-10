0eac5eca8dc039e7719908cd880777e1ac325801
Juan I. Sabbione <sabbione@ualberta.ca>
2016-06-09 20:40:18 -0600

    Add an example given keywords values

--- src/Modelling/Ricker.jl
+++ src/Modelling/Ricker.jl
@@ -3,21 +3,18 @@
 
 Create a Ricker wavelet.
 
-# Arguments
-
-**Keyword arguments**
-
+# Keyword arguments
 * `dt::Real=0.002`: sampling interval in secs.
 * `f0::Real=20.0`: central frequency in Hz.
 
-# Example
+# Examples
 ```julia
-julia> w = Ricker(); plot(w);
+julia> w = Ricker(); plot(w)
+julia> w = Ricker(dt=0.004, f0=20); plot(w)
 ```
 
-**Reference**
-
-* Sheriff, Robert, 2002, Encyclopedic Dictionary of Applied Geophysics, fourth
+# Reference
+Sheriff, Robert, 2002, Encyclopedic Dictionary of Applied Geophysics, fourth
 ed.: Society of Exploration Geophysicists. Geophysical Reference Series No. 13.
 """
 
