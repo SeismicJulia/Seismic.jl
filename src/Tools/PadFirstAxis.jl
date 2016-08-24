316eb55f4ccfcac7569e8aa7e9688570c190355c
Aaron Stanton <aaron.stanton@gmail.com>
2016-08-24 16:13:21 -0600

    Update PadFirstAxis.jl
    shortened the syntax

--- src/Tools/PadFirstAxis.jl
+++ src/Tools/PadFirstAxis.jl
@@ -20,10 +20,6 @@ function PadFirstAxis{T<:Real, N}(a::Array{T,N}, N1::Int)
     dims = size(a)
     n1 = dims[1]
     N1 > n1 || error("N1 must be greater than size(a,1)")
-    length(dims) == 1 ? nx = 1 : nx = *(dims[2:end]...)
-    b = zeros(T, N1, nx)
-    for j = 1:nx
-	b[1:n1, j] = a[:, j]
-    end
-    reshape(b, N1, dims[2:end]...)
+    b = zeros(T,N1,dims[2:end]...) 
+    b[1:dims[1],:] = a	
 end
