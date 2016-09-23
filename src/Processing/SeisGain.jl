2ee0b7aa29b59e5aa4ae358e37d1cb6db14391a0
Juan I. Sabbione <sabbione@ualberta.ca>
2016-09-23 17:16:06 -0600

    Start using v0.5

--- src/Processing/SeisGain.jl
+++ src/Processing/SeisGain.jl
@@ -7,13 +7,9 @@ Gain a group of traces.
 * `d::Array{Real,2}`: two dimensional data.
 
 # Keyword arguments
-<<<<<<< HEAD
-* `kind::String="time"`: if kind="time", gain = t.^a . * exp(-bt);
-=======
 * `dt::Real=0.002`: sampling interval in secs.
-* `kind::ASCIIString="time"`: if kind="time", gain = t.^a . * exp(-bt);
->>>>>>> 92289204c25c06c226c13809ff140b01632e3552
-                              if kind="agc", automatic gain control is applied.
+* `kind::String="time"`: if kind="time", gain = t.^a . * exp(-bt);
+                         if kind="agc", automatic gain control is applied.
 * `param::Vector{Real}=[2.0,0.0]`: if kind="time", param = [a,b];
                                    if kind="agc", param = [agc_gate]
 * `norm::Int=0`: `norm=0` no normalization; `norm=1` normalize each trace by
@@ -33,7 +29,7 @@ Credits: Juan I. Sabbione, Aaron Staton, Mauricio D. Sacchi, 2016
 
 """
 function SeisGain{Td<:Real,Tp<:Real
-                  }(d::Array{Td,2}; dt::Real=0.004, kind::ASCIIString="time",
+                  }(d::Array{Td,2}; dt::Real=0.004, kind::String="time",
                     param::Vector{Tp}=[2.0,0.0], norm::Int=0)
     
     nt = size(d,1)
